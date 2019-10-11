30faea1f5032215bc4b644c03d7a595a2239bba4
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-11 11:10:36 -0500

    get and post handlers working

--- models/project-model.js
+++ models/project-model.js
@@ -4,8 +4,15 @@ const config = require('../knexfile');
 const db = knex(config);
 
 module.exports = {
+    getProjects,
     addProject,
 };
+
+//
+//Get Projects
+function getProjects() {
+    return db('projects');
+}
 
 //
 //Create New Project
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const Projects = require('../models/project-model');
 
+router.get('/', async (req, res) => {
+    let projects = await Projects.getProjects();
+    res.status(200).json(projects);
+});
+
 router.post('/', validateProject, async (req, res) => {
     let project = req.body;
     let newProject = await Projects.addProject(project);
@@ -11,7 +16,7 @@ router.post('/', validateProject, async (req, res) => {
 //Middleware
 function validateProject(req, res, next) {
     let project = req.body;
-    if (!project || !project.name) {
+    if (!project || !project.project_name) {
         next('Missing parameters');
     }
     next();
