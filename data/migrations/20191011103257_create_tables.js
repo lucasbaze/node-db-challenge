dbfb18f997c8cf6fbb501f2dc27b9b74840c974e
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-11 10:49:08 -0500

    create migrations file and execute

---
+++ data/migrations/20191011103257_create_tables.js
@@ -0,0 +1,50 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('projects', t => {
+            t.increments('project_id');
+            t.string('project_name', 128).notNullable();
+            t.boolean('complete').defaultTo(false);
+        })
+        .createTable('resources', t => {
+            t.increments('resource_id');
+            t.string('resource_name', 128).notNullable();
+            t.string('description');
+        })
+        .createTable('project_resources', t => {
+            t.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('RESTRICT');
+            t.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('resource_id')
+                .inTable('resources')
+                .onUpdate('CASCADE')
+                .onDelete('RESTRICT');
+        })
+        .createTable('tasks', t => {
+            t.increments();
+            t.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('RESTRICT');
+            t.string('description').notNullable();
+            t.string('notes');
+            t.boolean('complete').defaultsTo(false);
+        });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects');
+};
